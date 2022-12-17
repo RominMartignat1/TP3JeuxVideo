8b857685af0174850802e0484794e1bcc33f1f67
RominMartignat <rominmart810@gmail.com>
2022-12-17 18:55:44 -0500

    un commit pour dire qu'il resterait les sons

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -10,13 +10,18 @@ public class SoundManager : MonoBehaviour
     [SerializeField] private AudioClip playerKilled;
     [SerializeField] private AudioClip gameMusic;
     [SerializeField] private AudioClip bulletSound;
+    [SerializeField] private AudioClip deathSound;
+    [SerializeField] private AudioClip powerUpSound;
 
 
     public static SoundManager Instance { get { return instance; } }
     public AudioClip PlayerJump { get { return playerJump; } }
     public AudioClip GameMusic { get { return gameMusic; } }
     public AudioClip FireBulletSound { get { return bulletSound; } }
-   
+    public AudioClip PlayerDeath { get { return deathSound; } }
+    public AudioClip PowerUp { get { return powerUpSound; } }
+
+
     // Start is called before the first frame update
     void Start()
     {
